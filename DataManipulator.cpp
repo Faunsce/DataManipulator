1bdc9d0a3263c6492bbae30635a0bea7c9c6b2b7
Dominik Rutter <dominik.rutter@gmail.com>
2023-10-21 20:35:49 -0400

    Boilerplate init

--- DataManipulator.cpp
+++ DataManipulator.cpp
@@ -1,12 +1,19 @@
-﻿// DataManipulator.cpp : Defines the entry point for the application.
-//
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
 
-#include "DataManipulator.h"
-
-using namespace std;
+#define CLEAR_SCREEN() std::cout << "\x1b[2J\x1b[H"
+#define PROGRAM_WAIT(x) std::this_thread::sleep_for(std::chrono::milliseconds(x));
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	std::cout << "Welcome to the Data manipulator!" << std::endl;
+
+	PROGRAM_WAIT(500);
+
+	CLEAR_SCREEN();
+
+	std::cout << "What would like like to do to the data today?" << std::endl;
+
+	std::cin.ignore(1);
 }
