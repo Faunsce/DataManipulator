5fd0eb85c5549d4add27601decc7af5d266b1b40
Dominik Rutter <dominik.rutter@gmail.com>
2023-10-21 20:25:43 -0400

    Add project files.

---
+++ DataManipulator.cpp
@@ -0,0 +1,12 @@
+﻿// DataManipulator.cpp : Defines the entry point for the application.
+//
+
+#include "DataManipulator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
